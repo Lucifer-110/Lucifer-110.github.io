9253891dc282abf047ca21bdf0451014e81c9e83
Lucifer-110 <162952318+Lucifer-110@users.noreply.github.com>
2024-07-04 12:14:46 +0800

    Update script.js
    Signed-off-by: Lucifer-110 <162952318+Lucifer-110@users.noreply.github.com>

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ $(document).ready(function(){
     });
 });
 
-function openContactOptions(productName) {
-    const options = `
-        <div class="contact-method">
-            <p>选择一个联系方法来购买 ${productName}:</p>
-            <a href="weixin://dl/chat?your_wechat_id"><i class="fab fa-weixin"></i></a>
-            <a href="tencent://message/?uin=your_qq_number&Site=Sambilan.com&Menu=yes"><i class="fab fa-qq"></i></a>
-            <a href="tg://resolve?domain=your_telegram_username"><i class="fab fa-telegram-plane"></i></a>
-            <a href="https://discordapp.com/users/your_discord_id"><i class="fab fa-discord"></i></a>
-        </div>
-    `;
-    const newWindow = window.open("", "_blank", "width=400,height=400");
-    newWindow.document.write(options);
-}
+//function openContactOptions(productName) {
+    //const options = `
+        //<div class="contact-method">
+            //<p>选择一个联系方法来购买 ${productName}:</p>
+            //<a href="weixin://dl/chat?your_wechat_id"><i class="fab fa-weixin"></i></a>
+            //<a href="tencent://message/?uin=your_qq_number&Site=Sambilan.com&Menu=yes"><i class="fab fa-qq"></i></a>
+           // <a href="tg://resolve?domain=your_telegram_username"><i class="fab fa-telegram-plane"></i></a>
+           // <a href="https://discordapp.com/users/your_discord_id"><i class="fab fa-discord"></i></a>
+       // </div>
+    //`;
+    //const newWindow = window.open("", "_blank", "width=400,height=400");
+   // newWindow.document.write(options);
+//}
